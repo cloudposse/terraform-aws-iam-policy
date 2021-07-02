d12db9b8c3f0aa789948af55a11c8a96fdf0a66c
nitrocode <nitrocode@users.noreply.github.com>
2021-07-02 15:47:16 -0400

    Update outputs.tf
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "json" {
-  description = "JSON body of the iam policy document"
+  description = "JSON body of the IAM policy document"
   value       = try(data.aws_iam_policy_document.this[0].json, null)
 }
