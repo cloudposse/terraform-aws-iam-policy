081b6cdfcf636b8d6c3fade7af21d91907398105
jbonnier <j.bonnier@gmail.com>
2021-08-05 08:41:05 -0400

    Fix typo with principals identifier

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ data "aws_iam_policy_document" "this" {
 
         content {
           type        = principals.value.type
-          identifiers = condiprincipalstion.value.identifiers
+          identifiers = principals.value.identifiers
         }
       }
 
