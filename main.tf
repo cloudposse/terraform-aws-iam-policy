a90cb76f0c521f4a00986f238de023aeadf0aa83
Veronika Gnilitska <veronika.gnilitska@gmail.com>
2023-06-14 13:10:39 +0300

    fix: support JSON inputs for policy document

--- main.tf
+++ main.tf
@@ -84,11 +84,11 @@ data "aws_iam_policy_document" "this" {
   lifecycle {
     precondition {
       condition     = var.iam_policy_statements == null || var.iam_policy == null
-      error_message = "Only 1 of var.iam_policy and var.iam_policy_statments may be used, preferably var.iam_policy."
+      error_message = "Only 1 of var.iam_policy and var.iam_policy_statements may be used, preferably var.iam_policy."
     }
     precondition {
-      condition     = var.iam_policy_statements != null || var.iam_policy != null
-      error_message = "Exactly 1 of var.iam_policy and var.iam_policy_statments may be used, preferably var.iam_policy."
+      condition     = var.iam_policy_statements != null || var.iam_policy != null || length(local.source_policy_documents) > 0
+      error_message = "Exactly 1 of var.iam_policy and var.iam_policy_statements may be used, preferably var.iam_policy. JSON source policy documents may be used in addition to either of these."
     }
   }
 }
